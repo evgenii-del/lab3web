0f2abbbe110142e8b6563cce1377000ec7cf416c
Khasanov <zkhasanovv@gmail.com>
2020-12-16 16:09:11 +0200

    Added grid and mobile layout

--- js/main.js
+++ js/main.js
@@ -16,3 +16,21 @@ const openCity = (evt, cityName) => {
 }
 
 document.getElementById("defaultOpen").click();
+
+const onDropdown = () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
